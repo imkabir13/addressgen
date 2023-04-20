55292c3147b773f0b38c3f1271ce4c1ad811216d
Imran Kabir <imkabir13@hotmail.com>
2023-04-20 00:29:10 -0400

    feat: add expample using zip code api

---
+++ cypress/e2e/exByZipCodeAPI.cy.ts
@@ -0,0 +1,21 @@
+describe("Random address test", () => {
+  it("should retrieve a random address within a zip code area", () => {
+    const apiKey = "your_api_key";
+    const zipCode = "90210";
+
+    cy.request(
+      `https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=${zipCode}&minimumradius=0&maximumradius=5&key=${apiKey}`
+    ).then((response) => {
+      const zipCodes = response.body;
+      const randomZipCode =
+        zipCodes[Math.floor(Math.random() * zipCodes.length)].Code;
+
+      cy.request(
+        `https://api.zip-codes.com/ZipCodesAPI.svc/1.0/GetZipCodeDetails/${randomZipCode}?key=${apiKey}`
+      ).then((response) => {
+        const address = response.body;
+        cy.log(JSON.stringify(address));
+      });
+    });
+  });
+});
